16d08e5c7264474ed6dd03abef9f6ddf97baac0a
adarshchauhan139 <adarshchauhan54321@gmail.com>
2026-03-19 00:31:01 +0530

    Day 2 - Added loading status, button control, and error handling

--- app.js
+++ app.js
@@ -4,37 +4,45 @@ const result = document.getElementById("result")
 
 
 searchBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    let username = inputValue.value.trim();
+    event.preventDefault(); //to stop the form from reloading or refreshing.
+    let username = inputValue.value.trim(); //use value to get the user typed data and use trim to remove leading and trailing white space.
 
+    // Quick check if user doesn't type anything then there is no need to run the function.
     if(!username){
         result.innerText = "Please enter a username";
         return;
     }
 
+    result.innerText = "Loading...";
+    searchBtn.disabled = true; //Button Disabled.
+
     fetchUser(username);
-})
+});
+
 
 function fetchUser(username){
-    let userUrl = `https://api.github.com/users/${username}`;
+    let userUrl = `https://api.github.com/users/${username}`; //Made a custon link from template literal string method.
 
-    fetch(userUrl)
-      .then(function(response){
-        return response.json();
+    // searchBtn.disabled = true;
+
+    fetch(userUrl) //this fetch data from the API
+      .then(function(response){ //this .then catch the response from the API
+        return response.json(); //use .json() because response data is not in a usable manner so this method convert this into usable data format.
       })
-      .then(function(data){
-        result.innerHTML = "";
 
-        let card = document.createElement('div');
-        card.classList.add("user-card");
+      .then(function(data){ //and then this .then catch the response 
+        result.innerHTML = ""; //first clean the existing part from the result div.
+
+        let card = document.createElement('div'); //make a div to store user info.
+        card.classList.add("user-card"); //add class to easily give styling later.
 
         
-        if(data.message === "Not Found"){
-            let error = document.createElement('p')
-            error.innerText = "User not found";
+        if(data.message === "Not Found"){ //When API not able to find the user then it shows Not Found.
+            let error = document.createElement('p') //create para element when no user is exist.
+            error.innerText = "User not found"; //and print the text innerText.
 
-            card.appendChild(error);
-            result.appendChild(card);
+            card.appendChild(error); //and then append the error into card
+            result.appendChild(card); //and then card into result.
         } else {
             let userName = document.createElement('h3');
             userName.innerText = data.name || data.login;
@@ -51,8 +59,15 @@ function fetchUser(username){
             card.append(userName, followers, userAvtar, repoCount);
             result.appendChild(card);
         }
-        
 
-        console.log(data);
+        searchBtn.disabled = false; //Button Enabled.
+
+        // console.log(data);
+      })
+
+      .catch(function(error){
+        result.innerText = "Something went wrong";
+        // console.log("Something went wrong");
+        searchBtn.disabled = false; //Button Enabled.
       })
 }
